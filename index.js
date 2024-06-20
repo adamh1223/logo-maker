217c31dff667e522ed7184e0bd31fa0867d30443
Adamh1223 <adam@hussmedia.io>
2024-06-19 23:06:03 -0700

    generate svg file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const { Circle, Triangle, Square } = require("./lib/shapes");
+const SVG = require("./lib/svg");
 
 const questions = [
   // validate, only allow 3 chars
@@ -8,30 +9,44 @@ const questions = [
     type: "input",
     name: "text",
     message: "Type in up to 3 characters to display on your object",
+    validate: function (input) {
+      if (input.length > 3) {
+        return "Text must be 3 characters or less.";
+      }
+      return true;
+    },
   },
   // add a list of colors for the user to choose from
   {
     type: "list",
     name: "color",
     message: "What color would you like the text to be?",
-    choices: ["red", "green", "blue"],
+    choices: [
+      "red",
+      "green",
+      "blue",
+      "yellow",
+      "orange",
+      "purple",
+      "brown",
+      "gray",
+    ],
   },
   {
     type: "list",
     name: "shape",
-    message: "what shape would you like to create?",
+    message: "What shape would you like to create?",
     choices: ["Circle", "Triangle", "Square"],
   },
 ];
 
 function init() {
   inquirer.prompt(questions).then((answers) => {
-    // writeToFile("README.md", markdown);
     console.log(answers);
 
     let svgShape;
 
-    // based on the type of object the user chooses, you will have to create an appropriate class
+    // Based on the type of object the user chooses, create the appropriate class
     switch (answers.shape) {
       case "Circle":
         svgShape = new Circle();
@@ -51,6 +66,20 @@ function init() {
     }
 
     svgShape.setColor(answers.color);
+
+    const svg = new SVG();
+    svg.setShape(svgShape);
+    svg.setText(answers.text, answers.color);
+
+    const svgContent = svg.render();
+
+    fs.writeFile("output.svg", svgContent, (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+      } else {
+        console.log("SVG file created successfully!");
+      }
+    });
   });
 }
 
